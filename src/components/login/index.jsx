520865a619cd9898c90a33ae127404ca73c9349a
poland <741500199@qq.com>
2020-01-13 20:13:27 +0800

    请求登录功能

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
-import { Form, Input, Button, Icon } from 'antd';
+import { Form, Input, Button, Icon, message } from 'antd';
+import axios from 'axios'
 
 // 图片必须引入，才会被webpack打包
 import logo from './logo.png';
@@ -29,9 +29,46 @@ class Login extends Component {
         // 必须要调用
         callback();
     };
-
+    //发送请求，请求登录
+  login = (e) => {
+    e.preventDefault()
+    /*通过validateFields方法来校验表单并收集表单数据  
+    参数err:表示当表单校验失败，值为对象，表单校验成功，值为null
+    参数value：表示用户输入的数据
+*/
+    this.props.form.validateFields((err, value) => {
+      //判断是否校验成功
+      if (!err) {
+        //获取数据
+        const { username, password } = value
+        //发送请求
+        axios.post('/api/login', { username, password })
+          .then((response) => {
+          //请求成功
+            if (response.data.status === 0) {
+              //登录成功，跳转页面
+              //return <Redirect to="/" /> 用于render方法中，路由链接跳转
+              //this.props.history.push('/')  可以返回登录页面
+              this.props.history.replace('/')  // 不能返回登录页面
+            } else {
+              //登录失败，弹出提示，且清空密码
+              message.error(response.data.msg)
+              this.props.form.resetFields(['password']);
+            }
+            console.log(response); 
+          })
+          .catch((err) => {
+            //请求失败，弹出提示，清空密码
+            message.error('网络错误');
+            this.props.form.resetFields(['password']);
+        })
+      }
+    })
+  }
+     
+     
     render() {
-            // getFieldDecorator 高阶组件：用来表单校验
+            // 用来表单校验
             const { getFieldDecorator } = this.props.form;
 
         return (
@@ -42,7 +79,7 @@ class Login extends Component {
                 </header>
            <section className='login-section' >
                 <h2 > 用户登录 </h2>
-                <Form className='login-form' >
+                <Form className='login-form' onSubmit={this.login} >
                     <Form.Item >
                     {getFieldDecorator('username', {
                         rules: [              
@@ -70,16 +107,18 @@ class Login extends Component {
                                 validator: this.validator
                             }]
                         })(
-                            <Input prefix = { < Icon type = 'lock'
+                          <Input type='password'
+                            prefix={< Icon type='lock'
                                 style = {
                                     { color: 'rgba(0,0,0,.25)' }}/>}
                                 placeholder = '密码' / >
                             )
                         } 
                         </Form.Item> 
-                        <Form.Item >
-                        <Button className = 'login-form-btn'
-                        type = 'primary'>
+                        <Form.Item>
+                  <Button className='login-form-btn'
+                    htmlType='submit' /* 触发原生事件 */
+                    type='primary'>
                         登录 </Button> 
                         </Form.Item> 
                         </Form> 
